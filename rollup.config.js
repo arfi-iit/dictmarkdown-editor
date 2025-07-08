409f1cd1b2676cf9b8426239f67218bf2a9cc4a0
RePierre <petru.rebeja@gmail.com>
2025-07-08 17:25:47 +0300

    Updated rollup config and changed package type to module

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,13 @@
-// const commonjs = require("@rollup/plugin-commonjs");
-// const resolve = require("@rollup/plugin-node-resolve");
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "public/bundle.js",
-    format: "iife", // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: true,
-  },
-  plugins: [
-    resolve(), // tells Rollup how to find modules in node_modules
-    // commonjs(), // convertscommonJS modules to ES modules
+  output: [
+    {
+      file: "public/bundle.js",
+      format: "iife",
+      sourcemap: true,
+    },
   ],
+  plugins: [nodeResolve()],
 };
