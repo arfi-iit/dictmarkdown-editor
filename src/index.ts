4c2506e9a25ebe06d7006a3063db08dd63c872e6
RePierre <petru.rebeja@gmail.com>
2026-01-27 20:41:34 +0200

    Added functions to get and set the text of the editor

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,8 @@ import { operationsPanel } from "./editor-panel";
 import { formattingUpdateListener } from "./editor-panel";
 import { editorPanelKeymap } from "./editor-panel";
 
-const initialText = `**Z** s. m. invar. A treizeci şi una^1^, respectiv ultima_2_ literă a
-alfabetului limbii române, şi sunetul corespunzător (consoană
-fricativă, dentală, sonoră). @Cf. DDRF, ŞĂINEANU^2^, RESMERIŢĂ, D.,
-CADE.@ *Sunt risipiţi … pe toată întinderea alfabetului, de la „a“
-până la „zed“.* @VINEA, L. I, 137, cf. DL, DM, DER.@ *A fost dezolat
-într-o zi când i se atrăsese atenţia despre o altă literă că este
-„zet“.* @ROMÂNIA LITERARĂ, 1970, nr. 72, 183, cf. M. D. ENC., DEX.@
-**◊** $Loc. adj. şi adv.$ **De la A la Z(ed)** (care este) de la un
-capăt la altul. @Cf. DEX.@
-- Pronunţat: *ze* şi (în expresii) *zet*, (ieşit din uz) *zed.*
-`;
-
-const targetElement:any = document.querySelector("#editor");
-new EditorView({
-  doc: initialText,
+const targetElement: any = document.querySelector("#editor");
+export const editor = new EditorView({
   extensions: [
     minimalSetup,
     dictMarkdownLanguage,
@@ -33,3 +20,19 @@ new EditorView({
   ],
   parent: targetElement
 });
+
+
+export function setEditorText(text: string) {
+  const transaction = editor.state.update({
+    changes: {
+      from: 0,
+      to: editor.state.doc.length,
+      insert: text
+    }
+  });
+  editor.dispatch(transaction);
+};
+
+export function getEditorText() {
+  return editor.state.doc.toString();
+}
