fa00fa83f179107d4f03a854835c2f024b7949f3
RePierre <petru.rebeja@gmail.com>
2026-01-31 17:25:40 +0200

    Split rollup output into two separate targets

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "public/dictmarkdown-editor.js",
+const plugins = [nodeResolve(), typescript()];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "public/dictmarkdown-editor.iffe.js",
       format: "iife",
       sourcemap: true,
       name: "DictmarkdownEditor",
     },
-  ],
-  plugins: [nodeResolve(), typescript()],
-};
+    plugins: plugins,
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "public/dictmarkdown-editor.esm.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: plugins,
+  },
+];
